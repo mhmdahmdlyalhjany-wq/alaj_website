46ddd6c7aafbe7157331396426194cd66e29de62
mhmdahmdlyalhjany-wq <mhmdahmdlyalhjany@gmail.com>
2026-04-06 01:23:04 +0300

    Add files via upload

---
+++ holding_website/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Holding Website",
+    "version": "17.0.1.0.0",
+    "category": "Website/Website",
+    "summary": "Holding website pages and layout",
+    "author": "Cloud Systems",
+    "license": "LGPL-3",
+    "depends": [
+        "website",
+        "website_sale",
+        "website_sale_wishlist",
+    ],
+    "data": [
+        "data/holding_website_record.xml",
+        "data/holding_layout.xml",
+        "data/holding_homepage.xml",
+        "data/holding_about_page.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
